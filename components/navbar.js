5dd4222ad28383a881fdb88d29a53f029589055c
RUZ4KI <rushikate23@gmail.com>
2023-06-03 00:23:19 +0530

    fix navbar bug

--- components/navbar.js
+++ components/navbar.js
@@ -13,7 +13,7 @@ const LinkItem = ({ href, path, target, children, ...props }) => {
         <NextLink href={href} passHref scroll={false}>
             <Link
                 p={2}
-                bg={active ? 'glassTeal' : undefined}
+                bg={active ? 'grassTeal' : undefined}
                 color={active ? '#202023' : inactiveColor}
                 target={target}
                 {...props}
